679115a0a58a533afdd6cf1914f2798a771e5217
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-07-06 18:43:41 -0300

    Enhance AuthLayout with illustration and descriptive text; update index.html and index.css for styling improvements

--- src/ui/layouts/AuthLayout.tsx
+++ src/ui/layouts/AuthLayout.tsx
@@ -1,10 +1,24 @@
 import { Outlet } from 'react-router';
+import illustration from '../../assets/illustration.png';
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <Outlet />
+    <div className='flex w-full h-full'>
+      <div className='w-1/2 h-full'>
+        <Outlet />
+      </div>
+      <div className='w-1/2 h-full flex justify-center items-center p-8 relative'>
+        <img
+          src={illustration}
+          className='object-cover rounded-[32px] w-full h-full max-w-[656px] max-h-[960px] select-none'
+        />
+        <div className='bottom-8 max-w-[656px] absolute p-10 rounded-b-[32px] bg-white'>
+          <p>
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
