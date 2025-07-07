fe1fd45097942f21a12fb2397cf106608fceac9d
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-07-07 12:35:09 -0300

    feat: add Logo component and SVG logo
    - Introduced a new Logo component that renders an SVG logo.
    - Added logo.svg file containing the SVG markup for the logo.
    - Updated AuthLayout to utilize the new Logo component, enhancing the layout with a consistent branding element.
    - Adjusted styles in index.css for better color consistency across the application.

--- src/ui/layouts/AuthLayout.tsx
+++ src/ui/layouts/AuthLayout.tsx
@@ -1,19 +1,24 @@
 import { Outlet } from 'react-router';
 import illustration from '../../assets/illustration.png';
+import { Logo } from '../components/Logo';
 
 export function AuthLayout() {
   return (
     <div className='flex w-full h-full'>
-      <div className='w-1/2 h-full'>
-        <Outlet />
+      <div className=' w-full lg:w-1/2 h-full flex flex-col justify-center items-center gap-16'>
+        <Logo className='h-6 text-gray-500' />
+        <div className='w-full max-w-[504px] px-8 '>
+          <Outlet />
+        </div>
       </div>
-      <div className='w-1/2 h-full flex justify-center items-center p-8 relative'>
+      <div className='hidden w-1/2 h-full lg:flex justify-center items-center p-8 relative'>
         <img
           src={illustration}
           className='object-cover rounded-[32px] w-full h-full max-w-[656px] max-h-[960px] select-none'
         />
         <div className='bottom-8 max-w-[656px] absolute p-10 rounded-b-[32px] bg-white'>
-          <p>
+          <Logo className='text-teal-900 h-8' />
+          <p className='text-gray-700 font-medium text-xl mt-6'>
             Gerencie suas finanças pessoais de uma forma simples com o fincheck,
             e o melhor, totalmente de graça!
           </p>
